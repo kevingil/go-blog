a0de5af09c104ab3871cc346386e3d7ce8a0be4a
kevingil <kevin_gil@icloud.com>
2023-09-24 11:31:52 -0700

    docker

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,4 +1,4 @@
 wait-for "${MYSQL_HOST}:${MYSQL_PORT}" -- "$@"
 
 # Watch your .go files and invoke go build if the files changed.
-CompileDaemon --build="go build -o main main.go"  --command=./main
+#app-app --build="go build -o main main.go"  --command=./main
