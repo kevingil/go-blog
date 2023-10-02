5bda5cbd39a72cca41304f59d45156da7948736d
kevingil <kevin_gil@icloud.com>
2023-10-02 00:02:41 -0700

    controller

--- app/controllers/moderatorjs.go
+++ app/controllers/moderatorjs.go
@@ -8,10 +8,11 @@ import (
 	"github.com/kevingil/blog/app/templates"
 )
 
-// This just handles the page, ModAI is written in JS
+// This just handles the page, Moderator is written in JS
 func ModeratorJS(w http.ResponseWriter, r *http.Request) {
 	var response bytes.Buffer
-	if err := templates.Tmpl.ExecuteTemplate(&response, "moderatorai.htmx", data); err != nil {
+	// Define the data variable
+	if err := templates.Tmpl.ExecuteTemplate(&response, "moderatorai.htmx", nil); err != nil {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
