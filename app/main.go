2d07b1c1a912e3207010ea561ce8462ba6372d9b
kevingil <kevin_gil@icloud.com>
2023-11-20 13:42:20 -0800

    initialize all tables if connected + not exists

--- app/main.go
+++ app/main.go
@@ -64,23 +64,160 @@ func init() {
 		time.Sleep(retryInterval)
 	}
 
-	// This will just log errors, won't return error
-	sys_checks(models.Db)
+	initDb(models.Db)
 }
 
-func sys_checks(db *sql.DB) {
-	users_err := check_table(db, "users")
-	if users_err != nil {
-		log.Fatal(users_err)
+// inintDb will check for necessary tables and create them if not exists
+func initDb(db *sql.DB) {
+	testSetup(db)
+	users := testTable(db, "users")
+	if users != nil {
+		// Create users table
+		_, err := db.Exec(`
+					CREATE TABLE IF NOT EXISTS users (
+					id int(11) NOT NULL AUTO_INCREMENT,
+					name varchar(64) NOT NULL,
+					email varchar(320) NOT NULL,
+					password varchar(255) NOT NULL,
+					PRIMARY KEY (id),
+					UNIQUE KEY email (email)
+				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+			`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print(users)
 	}
-	articles_err := check_table(db, "articles")
-	if articles_err != nil {
-		log.Fatal(articles_err)
+	articles := testTable(db, "articles")
+	if articles != nil {
+
+		// Create articles table
+		_, err := db.Exec(`
+					CREATE TABLE IF NOT EXISTS articles (
+					id int(11) NOT NULL AUTO_INCREMENT,
+					image varchar(255) DEFAULT NULL,
+					slug varchar(255) NOT NULL,
+					title varchar(60) NOT NULL,
+					content text NOT NULL,
+					author int(11) NOT NULL,
+					created_at datetime NOT NULL,
+					PRIMARY KEY (id),
+					UNIQUE KEY slug (slug),
+					KEY author (author)
+				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+			`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print(articles)
+	}
+	skills := testTable(db, "skills")
+	if skills != nil {
+
+		// Create SKILLS table
+		_, err := db.Exec(`
+					CREATE TABLE IF NOT EXISTS skills (
+					id int(11) NOT NULL AUTO_INCREMENT,
+					name varchar(60) NOT NULL,
+					logo text NOT NULL,
+					textcolor varchar(255) NOT NULL,
+					fillcolor varchar(255) NOT NULL,
+					bgcolor varchar(255) NOT NULL,
+					author int(11) NOT NULL,
+					PRIMARY KEY (id),
+					KEY author (author)
+				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+			`)
+		skills := "skills table created"
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print(skills)
+	}
+	projects := testTable(db, "projects")
+	if projects != nil {
+
+		// Create PROJECTS table
+		_, err := db.Exec(`
+					CREATE TABLE IF NOT EXISTS projects (
+					id int(11) NOT NULL AUTO_INCREMENT,
+					title varchar(255) NOT NULL,
+					description varchar(255) NOT NULL,
+					url varchar(255) NOT NULL,
+					image varchar(255) DEFAULT NULL,
+					classes varchar(255) DEFAULT NULL,
+					author int(11) NOT NULL,
+					PRIMARY KEY (id),
+					KEY author (author)
+				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+			`)
+		projects := "projects table created"
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Print(projects)
 	}
 }
 
-func check_table(db *sql.DB, name string) error {
-	// Check users table
+func testSetup(db *sql.DB) {
+
+	// Check if the time zone is already set
+	rowsTimezone, err := db.Query("SELECT @@session.time_zone")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rowsTimezone.Close()
+
+	var dbTimeZone string
+	for rowsTimezone.Next() {
+		err := rowsTimezone.Scan(&dbTimeZone)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// If not, set to PST
+	if dbTimeZone == "" {
+		_, err := db.Exec("SET time_zone = '-08:00';")
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Time zone initialized.")
+	} else {
+		log.Printf("Time zone is set to: %s\n", dbTimeZone)
+	}
+
+	// Check if the SQL_MODE is already set
+	rowsMode, err := db.Query("SELECT @@session.sql_mode")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rowsMode.Close()
+
+	var sqlMode string
+	for rowsMode.Next() {
+		err := rowsMode.Scan(&sqlMode)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// If not, set to NO_AUTO_VALUE_ON_ZERO
+	if sqlMode == "" {
+		_, err := db.Exec("SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';")
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("SQL_MODE initialized.")
+	} else {
+		log.Printf("SQL_MODE is set to: %s\n", sqlMode)
+	}
+
+}
+
+// testTable will test if a table exists
+func testTable(db *sql.DB, name string) error {
 	query := fmt.Sprintf("SHOW TABLES LIKE '%s'", name)
 	rows, err := db.Query(query)
 	if err != nil {
@@ -89,84 +226,9 @@ func check_table(db *sql.DB, name string) error {
 	defer rows.Close()
 
 	if rows.Next() {
-		fmt.Printf("'%s' table OK \n", name)
+		log.Printf("'%s' table OK \n", name)
 		return nil // Table exists
 	}
-
-	return fmt.Errorf("table '%s' does not exist", name)
-
-	/*
-		_, err = db.Exec("SET time_zone = '+00:00';")
-		if err != nil {
-			return err
-		}
-
-		// Create articles table
-		_, err = db.Exec(`
-			CREATE TABLE IF NOT EXISTS articles (
-				id int(11) NOT NULL,
-				image varchar(255) DEFAULT NULL,
-				slug varchar(255) NOT NULL,
-				title varchar(60) NOT NULL,
-				content text NOT NULL,
-				author int(11) NOT NULL,
-				created_at datetime NOT NULL,
-				PRIMARY KEY (id),
-				UNIQUE KEY slug (slug),
-				KEY author (author)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-		`)
-		if err != nil {
-			return err
-		}
-
-		// Create users table
-		_, err = db.Exec(`
-			CREATE TABLE IF NOT EXISTS users (
-				id int(11) NOT NULL,
-				name varchar(64) NOT NULL,
-				email varchar(320) NOT NULL,
-				password varchar(255) NOT NULL,
-				PRIMARY KEY (id),
-				UNIQUE KEY email (email)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-		`)
-		if err != nil {
-			return err
-		}
-
-		// Add indexes for articles table
-		_, err = db.Exec(`
-			ALTER TABLE articles
-			ADD PRIMARY KEY (id),
-			ADD UNIQUE KEY slug (slug),
-			ADD KEY author (author);
-		`)
-		if err != nil {
-			return err
-		}
-
-		// Add indexes for users table
-		_, err = db.Exec(`
-			ALTER TABLE users
-			ADD PRIMARY KEY (id),
-			ADD UNIQUE KEY email (email);
-		`)
-		if err != nil {
-			return err
-		}
-
-		// Set AUTO_INCREMENT for articles table
-		_, err = db.Exec("ALTER TABLE articles MODIFY id int(11) NOT NULL AUTO_INCREMENT;")
-		if err != nil {
-			return err
-		}
-
-		// Set AUTO_INCREMENT for users table
-		_, err = db.Exec("ALTER TABLE users MODIFY id int(11) NOT NULL AUTO_INCREMENT;")
-		if err != nil {
-			return err
-		}
-	*/
-
+	error := "'" + name + "' table ERROR \n"
+	return fmt.Errorf(error)
 }
