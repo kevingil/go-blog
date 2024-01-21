6b5b3c55783d2a30c0b3bfbf5fab66734f47629a
kevingil <kevin_gil@live.com>
2024-01-21 03:17:34 -0800

    update home and animations

---
+++ app/static/main.js
@@ -0,0 +1,40 @@
+// Header styling
+window.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY;
+    var element = document.querySelector('.scrollfade');
+    if (scrollPosition > 30) {
+        element.classList.add('scrolled');
+    } else {
+        element.classList.remove('scrolled');
+    }
+});
+// Animation observer
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        } else {
+            entry.target.classList.remove('animate');
+        }
+    });
+});
+
+const observeElements = (elements) => {
+    elements.forEach((element) => {
+        observer.observe(element);
+    });
+};
+
+const hiddenElements = document.querySelectorAll('.hide');
+observeElements(hiddenElements);
+
+// Animate newly loaded elements
+htmx.onLoad(function(elt) {
+    const newHiddenElements = elt.querySelectorAll('.hide');
+    observeElements(newHiddenElements);
+});
+
+// HTMX on hx-swap, scroll to top, no animation
+document.addEventListener('htmx:afterSwap', function (event) {
+    window.scrollTo(0, 0);
+});
