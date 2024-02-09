cd313de1f86ac9793f79582fec1698968efe8cf3
kevingil <kevin_gil@live.com>
2024-02-08 23:47:03 -0800

    colors

--- app/static/main.js
+++ app/static/main.js
@@ -26,6 +26,58 @@ const observeElements = (elements) => {
     });
 };
 
+// Card stack animations
+const cardStackObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate-card-stack');
+        } else {
+            entry.target.classList.remove('animate-card-stack');
+        }
+    });
+});
+
+const observeCardStack = (elements) => {
+    elements.forEach((element) => {
+        cardStackObserver.observe(element);
+    });
+};
+
+
+// Card animations
+const c1Observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate-c1');
+        } else {
+            entry.target.classList.remove('animate-c1');
+        }
+    });
+});
+
+const observeC1 = (elements) => {
+    elements.forEach((element) => {
+        c1Observer.observe(element);
+    });
+};
+
+// Card animations
+const c2Observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate-c2');
+        } else {
+            entry.target.classList.remove('animate-c2');
+        }
+    });
+});
+
+const observeC2 = (elements) => {
+    elements.forEach((element) => {
+        c2Observer.observe(element);
+    });
+};
+
 // Card animations
 const cardObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -43,8 +95,6 @@ const observeCards = (elements) => {
     });
 };
 
-
-
 // Combined onLoad function
 htmx.onLoad(function (elt) {
     // Text animations
@@ -54,6 +104,19 @@ htmx.onLoad(function (elt) {
     // Card animations
     const hiddenCards = elt.querySelectorAll('.hide-card');
     observeCards(hiddenCards);
+
+
+    // Card animations
+    const hiddenCardStack = elt.querySelectorAll('.hide-cards');
+    observeCardStack(hiddenCardStack);
+
+    // C1 animations
+    const hiddenC1 = elt.querySelectorAll('.fade-c1');
+    observeC1(hiddenC1);
+
+    // C2 animations
+    const hiddenC2 = elt.querySelectorAll('.fade-c2');
+    observeC2(hiddenC2);
 
     // Home animations
     const hiddenHomeCards = elt.querySelectorAll('.hide-card-home');
