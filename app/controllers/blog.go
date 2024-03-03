8986cc23d6947136ac9cbf21199a7bbe94772168
kevingil <kevin_gil@live.com>
2024-03-03 01:25:28 -0800

    add open graph tags

--- app/controllers/blog.go
+++ app/controllers/blog.go
@@ -11,6 +11,39 @@ import (
 	"github.com/kevingil/blog/app/views"
 )
 
+func Publish(w http.ResponseWriter, r *http.Request) {
+	permission(w, r)
+	cookie := getCookie(r)
+	user := Sessions[cookie.Value]
+	if user != nil {
+		data.Articles = user.FindArticles()
+	}
+
+	Hx(w, r, "dashboard", "publish", data)
+}
+
+func EditArticle(w http.ResponseWriter, r *http.Request) {
+	permission(w, r)
+	cookie := getCookie(r)
+	user := Sessions[cookie.Value]
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+
+	data.Article = &models.Article{
+		Image:   "",
+		Title:   "",
+		Content: "",
+		IsDraft: 0,
+	}
+
+	if user != nil && id != 0 {
+		data.Article = user.FindArticle(id)
+		data.Tags = data.Article.FindTags()
+	}
+
+	Hx(w, r, "main_layout", "edit_article", data)
+
+}
+
 // Blog post
 func Blog(w http.ResponseWriter, r *http.Request) {
 	pageStr := r.URL.Query().Get("page")
