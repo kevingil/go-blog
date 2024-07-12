fe70f4f7af1c51d917cfea2342064129e3d3bb5c
kevingil <kevin_gil@live.com>
2024-07-11 21:01:57 -0700

    add dashboard chart

--- internal/server/server.go
+++ internal/server/server.go
@@ -58,6 +58,7 @@ func Serve() {
 	// User admin
 	app.Get("/admin", controllers.AdminPage)
 	app.Get("/analytics/visits", controllers.GetSiteVisits)
+	app.Get("/api/site-visits-chart", controllers.GetSiteVisitsChart)
 
 	// Edit articles, delete, or create new
 	// View posts, preview drafts
